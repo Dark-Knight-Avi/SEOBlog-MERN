5f7b7f225abd13dabc519f9482979e3dccd8008c
Aritra Karmakar <64675840+Dark-Knight-Avi@users.noreply.github.com>
2023-02-20 12:49:04 +0530

    Add: Backend - Server, Database, Router, Controller

---
+++ backend/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const router = require("./router")
+require("dotenv").config();
+
+mongoose.set('strictQuery', false)
+
+//App
+const app = express();
+
+//database connection
+// mongoose.connect(process.env.DATABASE_CLOUD, { useNewUrlParser: true }).then(() => console.log("Database connection established in Cloud !!")).catch((e) => console.log("Error connecting to database: ", e))
+mongoose.connect(process.env.DATABASE_LOCAL, { useNewUrlParser: true}).then(() => console.log("Database connection established in Local !!")).catch((e) => console.log("Error connecting to database: ", e))
+
+//Middlewares
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//cors
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+// routes middleware
+app.use('/api', router)
+
+
+//port
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+})
